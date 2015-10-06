166d3a92ea1e4b752c8cb8fde2a55ca779c3f0c0
Antoine Lucas <git.alucas@gmail.com>
2015-10-07 00:07:09 +0200

    Add AppStore

---
+++ src/store/AppStore.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var assign = require("object-assign");
+var EventEmitter = require("events").EventEmitter;
+
+var Dispatcher = require("../dispatcher/Dispatcher");
+var ActionTypeConstant = require("../constant/ActionTypeConstant");
+
+var STORE_EVENT = "appStore";
+
+var _color = [1, 1];
+
+function _setColor(color) {
+  _color = color;
+}
+
+var AppStore = assign({}, EventEmitter.prototype, {
+  getColor: function() {
+    return _color;
+  },
+
+  emitChange: function() {
+    this.emit(STORE_EVENT);
+  },
+
+  addChangeListener: function(callback) {
+    this.on(STORE_EVENT, callback);
+  },
+
+  removeChangeListener: function(callback) {
+    this.removeListener(STORE_EVENT, callback);
+  }
+});
+
+Dispatcher.register(function(action) {
+  switch(action.actionType) {
+    case ActionTypeConstant.SET_COLOR:
+      _setColor(action.color);
+      AppStore.emitChange();
+      break;
+
+    default:
+      // no opp
+      break;
+  }
+});
+
+module.exports = AppStore;
