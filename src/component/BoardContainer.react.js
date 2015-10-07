23fcbed7b95e3075a18e82936a42871b4ddb6294
Antoine Lucas <git.alucas@gmail.com>
2015-10-07 23:36:45 +0200

    Set color on the board

--- src/component/BoardContainer.react.js
+++ src/component/BoardContainer.react.js
@@ -1,17 +1,27 @@
+"use strict"
+
 var React = require("react");
+
+var ColorConstant = require("../constant/ColorConstant");
 
 var BoardContainer = React.createClass({
   render: function() {
-    return <div className="boardContainer">
-        {this.props.board.map(function(row, i) {
-          return <div className="boardRow" key={i}>
-              {row.map(function(column, i) {
-                return <div className="boardColumn" key={i} style={{backgroundColor: "white"}}>
-                  </div>;
-              })}
+    return <div className="boardContainer">{
+      this.props.board.map(function(row, i) {
+        return <div className="boardRow" key={i}>{
+          row.map(function(column, j) {
+            return <div
+                      key={j}
+                      className={"boardColumn " + ColorConstant.CLASSNAME[column]}
+                      onClick={function() {this._handleClick(i, j)}.bind(this)}>
             </div>;
-        })}
+          }.bind(this))}
+        </div>;
+      }.bind(this))}
     </div>;
+  },
+
+  _handleClick: function(rowIndex, columnIndex) {
   }
 })
 
