3e36f2f72479b365fa2db1d1e019b6ac200c93bd
Antoine Lucas <git.alucas@gmail.com>
2015-10-12 22:09:10 +0200

    Get bootstrap from npm

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import { Provider } from 'react-redux';
 import App from './js/component/App.react';
 import store from './js/store/store';
 
-import './stylesheet/style.css';
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+import './stylesheet/style.scss';
 
 React.render(
   <Provider store={store}>
